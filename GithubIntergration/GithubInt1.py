e1a716fc668fd30b3a087fc3078f1d3d582d7ab8
nesimon <nesimon@tcd.ie>
2017-12-14 13:14:48 +0000

    Extra code

--- GithubIntergration/GithubInt1.py
+++ GithubIntergration/GithubInt1.py
@@ -2,6 +2,7 @@ from github import Github
 
 # Input your own user name and password to displa info
 #g = Github("user", "password")
+# Or input your github access code
 g = Github("92cc88158b67be82b0dd4dabd1bf7b6ec4dce0bd")
 
 user = g.get_user()
