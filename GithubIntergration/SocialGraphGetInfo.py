7ea25bc3cf17790010252861b18a4cb7780f11a0
nesimon <nesimon@tcd.ie>
2017-12-14 19:26:19 +0000

    Finished Social Graph

--- GithubIntergration/SocialGraphGetInfo.py
+++ GithubIntergration/SocialGraphGetInfo.py
@@ -2,21 +2,27 @@ from github import Github
 import csv
 from itertools import zip_longest
 
+# Input your own user name and password to display info
+g = Github("username", "Password")
 # Or input your github access code to display info
-g = Github("873ce35699f41cdb3bd2bfe2145ac1697e99bfe0")
+#g = Github("Github Access Code")
+
+# This code gets the top 10 developers in the React repo of facebook and converts it to a csv file.
 user = g.get_user(login= "facebook")
 
 list1 = []
 list2 = []
 
 repo = user.get_repo("react")
+line = 0
 for contributors in repo.get_stats_contributors():
-      list1.append(contributors.author.login)
-      list2.append(contributors.total)
-
+    if line >= 91:
+        list1.append(contributors.author.login)
+        list2.append(contributors.total)
+    line +=1
 d = [list1, list2]
 export_data = zip_longest(*d, fillvalue = '')
-with open('Data.csv', 'w', encoding="ISO-8859-1", newline='') as myfile:
+with open('Data.csv', 'w', newline='') as myfile:
       wr = csv.writer(myfile)
       wr.writerow(("Developer", "Commits"))
       wr.writerows(export_data)
